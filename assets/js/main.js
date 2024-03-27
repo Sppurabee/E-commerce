55709de2e91d97c7d3ee6b243d03ba9eeaeddf74
Sppurabee <spayesha75@gmail.com>
2024-03-28 05:47:28 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+$( document ).ready(function() {
+    
+$('.responsive').slick({
+  dots: true,
+  arrows:true,
+  infinite: true,
+  speed: 1000,
+  autoplay:true,
+  poseonhover:true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll:2,
+        infinite: true,
+        dots: true,
+        
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+});
